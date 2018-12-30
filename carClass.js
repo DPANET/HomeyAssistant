a27422ccd15e98474afca0d0976a90a4fad897d5
omar <dpanet@hotmail.com>
2018-12-30 18:16:39 +0400

    first commit

---
+++ carClass.js
@@ -0,0 +1,53 @@
+﻿'use strict';
+var http = require('http');
+
+
+var person = { firstname: "omar", lastname: "Bamatraf", x: 4, y: 5 };
+var car = ["Nouf", "Leen"];
+car.push("Amna");
+function House() {
+    // let doorLock, light;
+    this.doorLock = "Nuki";
+    this.light = "Flex";
+    this.rent = 3000;
+
+}
+//exports.myHouseRent = new House();
+let myHouse = new House();
+
+console.log(myHouse.doorLock);
+console.log(myHouse.rent);
+console.log(car.length);
+
+//console.log(car.forEach(myArray));
+//console.log(Boolean(20 == 20));
+//if (person.x != 3) {
+//    console.log(person.x);
+
+//}
+//else {
+//    console.log("mi['/stake");
+//}
+////console.log(typeof (person.firstname).toString());
+//for (var x of car) {
+//    console.log(x);
+
+//}
+
+//var i = 0;
+//while (car[i]) {
+//    if (i == 0) {
+//        console.log(car[i]);
+//        i = 2;
+//        continue;
+//    }
+//    i += 1;
+//}
+//console.log('Hello world');
+//return;
+
+//function myArray(value) {
+//    console.log(value);
+
+//}
+
