d1607abfade69906447b855cd132d16424f3fa89
omar <dpanet@hotmail.com>
2019-01-23 19:51:31 +0400

    va

--- src/providers/prayer-provider.ts
+++ src/providers/prayer-provider.ts
@@ -4,30 +4,40 @@ import Debug = require('debug');
 const debug = Debug("app:startup");
 const to = require('await-to-js').default;
 import ramda = require('ramda');
-import { isNullOrUndefined } from 'util';
-import { IPrayerAdjustments, IPrayerLatitude, IPrayerMethods, IPrayerSchools, IPrayersSettings, IPrayersTime, IPrayers } from '../entities/prayers';
+import config = require('config');
+import { isNullOrUndefined, promisify } from 'util';
+import { IPrayerAdjustments , IPrayerLatitude, IPrayerMethods, IPrayerSchools, IPrayersSettings, IPrayersTime, IPrayers } from '../entities/prayers';
 
 export enum PrayerProviderName {
     PRAYER_TIME = "Prayer Time",
     APPLE = "Apple"
 
 }
+const prayerTimePaths = 
+{
+    latitude: 'apis.prayersAPI.latitude',
+    apiurl: 'apis.urls',
+    adjustment: 'settings.adjustments',
+    calculations: 'settings.calcuations',
+
+
+}
 const LocationErrorMessages =
 {
     BAD_INPUT: 'Location input provided are not valid',
-    NOT_FOUND: 'Location provided cannot be found, try again with different input',
+    LATITUDE_NOT_FOUND: 'Latitude record is not found, please try again',
     BAD_RESULT: 'Location provided results have caused unexpected error, please try different input',
     TIME_OUT: 'Connection cannot be made to location provider, please try again after a while'
 }
 export interface IPrayerProvider
 {
     getProviderName(): PrayerProviderName;
-    getPrayerLatitude(): Promise<IPrayerAdjustments>;
-    getPrayerLatitudeById(id:number): Promise<Array<IPrayerLatitude>>;
+    getPrayerLatitude(): Promise<Array<IPrayerLatitude>>;
+    getPrayerLatitudeById(index:number): Promise<IPrayerLatitude>;
     getPrayerMethods():Promise<Array<IPrayerMethods>>;
-    getPrayerMethodsById(id:number):Promise<IPrayerMethods>;
+    getPrayerMethodsById(index:number):Promise<IPrayerMethods>;
     getPrayerSchools():Promise<Array<IPrayerSchools>>;
-    getPrayerSchoolsById(id:number):Promise<IPrayerSchools>;
+    getPrayerSchoolsById(index:number):Promise<IPrayerSchools>;
     getPrayerSettings(): Promise<IPrayersSettings>;
     getPrayerTime(prayerSettings:IPrayersSettings):Promise<Array<IPrayers>>;
 }
@@ -40,27 +50,43 @@ abstract class PrayerProvider implements IPrayerProvider {
     getProviderName(): PrayerProviderName {
         return this._providerName;
     }   
-    abstract getPrayerLatitude(): Promise<IPrayerAdjustments>;
-    abstract getPrayerLatitudeById(id: number): Promise<IPrayerLatitude[]>;
+    abstract getPrayerLatitude(): Promise<Array<IPrayerLatitude>>;
+    abstract getPrayerLatitudeById(index:number): Promise<IPrayerLatitude>;
     abstract getPrayerMethods(): Promise<IPrayerMethods[]> ;
-    abstract getPrayerMethodsById(id:number):Promise<IPrayerMethods>;
+    abstract getPrayerMethodsById(index:number):Promise<IPrayerMethods>;
     abstract getPrayerSchools():Promise<Array<IPrayerSchools>>;
-    abstract getPrayerSchoolsById(id:number):Promise<IPrayerSchools>;
+    abstract getPrayerSchoolsById(index:number):Promise<IPrayerSchools>;
     abstract getPrayerSettings(): Promise<IPrayersSettings>;
     abstract getPrayerTime(prayerSettings:IPrayersSettings):Promise<Array<IPrayers>>;
 }
 
-class PrayerTimeProvider extends PrayerProvider
+export class PrayerTimeProvider extends PrayerProvider
 {
+    private _prayerLatitude:IPrayerLatitude;
+    private _prayerMethod:IPrayerMethods;
+    private _prayerSchools:IPrayerSchools;
+    private _prayerSettings:IPrayersSettings;
+    private _prayerTime:IPrayersTime;
+    private _prayers:IPrayers;
+
     constructor() {
         super(PrayerProviderName.PRAYER_TIME);
-
     }
-    getPrayerLatitude(): Promise<IPrayerAdjustments> {
-        throw new Error("Method not implemented.");
+   async getPrayerLatitude(): Promise<Array<IPrayerLatitude>> {
+       return await config.get<Array<IPrayerLatitude>>(prayerTimePaths.latitude);
     }   
-    getPrayerLatitudeById(id: number): Promise<IPrayerLatitude[]> {
-        throw new Error("Method not implemented.");
+   async getPrayerLatitudeById(index:number): Promise<IPrayerLatitude>
+    { let err, prayerLatitudeList:Array<IPrayerLatitude>, prayerLatitude: IPrayerLatitude;
+        let filterById = ramda.where({id: ramda.equals(index)});
+        [err,prayerLatitudeList] = await to(this.getPrayerLatitude());
+        if(err)
+        return Promise.reject(err);
+        prayerLatitude=ramda.filter<IPrayerLatitude>(filterById,prayerLatitudeList).pop();
+        if(!isNullOrUndefined(prayerLatitude))
+        return prayerLatitude;
+        else
+        return Promise.reject(LocationErrorMessages.LATITUDE_NOT_FOUND);
+              
     }
     getPrayerMethods(): Promise<IPrayerMethods[]> {
         throw new Error("Method not implemented.");
