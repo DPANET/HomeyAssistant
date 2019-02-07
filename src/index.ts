28aad01496f415e1bfe3f2f1e7c88e78646fb104
omar <dpanet@hotmail.com>
2019-02-07 21:31:12 +0400

    enter

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export * from './configurators/configuration'  // re-export all of its exports
 export * from './entities/location';
 export * from './entities/prayer';
+export * from './managers/event';
+export * from './managers/manager';
+export * from './providers/location-provider';
