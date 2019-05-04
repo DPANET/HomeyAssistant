8a2848c74735cab68255698c2d8bc27755f3cd40
omar <dpanet@hotmail.com>
2019-05-04 05:27:55 +0400

    get prayers app main

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@
 import prayer = require("./entities/prayer");
 import cg = require("./configurators/configuration");
 import * as manager from './managers/manager';
+import ramda from "ramda";
 let prayers: Array<object> =
     [{ prayerName: 'Fajr', prayerTime: "2019-01-31T05:46:00.000Z" },
     { prayerName: 'Sunrise', prayerTime: "2019-01-31T07:02:00.000Z" },
@@ -13,6 +14,25 @@ let prayers: Array<object> =
     { prayerName: 'Imsak', prayerTime: "2019-01-31T05:34:00.000Z" },
     { prayerName: 'Midnight', prayerTime: "2019-01-31T00:36:00.000Z" }];
 buildLocationObject().catch((err) => console.log(err));
+interface IPrayersView
+{
+    prayerDate:Date,
+    fajr:Date,
+    sunrise:Date,
+    dhuhr:Date,
+    asr:Date,
+    sunset:Date,
+    maghrib:Date,
+    isha:Date,
+    midnight:Date
+}
+function getPrayerView(prayers:prayer.IPrayers []):IPrayersView[]
+{
+    let prayersView:IPrayersView;
+    
+    console.log(ramda.pick(['prayersDate']));
+    return;
+}
 async function buildLocationObject() {
     try {
         // let prayerConfig: cg.IPrayersConfig = await new cg.Configurator().getPrayerConfig();
@@ -26,11 +46,37 @@ async function buildLocationObject() {
             .createPrayerTimeBuilder(null, null)
             .setLocationByAddress("Abu Dhabi","AE")
             .createPrayerTimeManager();
-            console.log(prayerManager.getPrayerLocation());
-            console.log(prayerManager.getPrayerAdjustmentsByPrayer(prayer.PrayersName.FAJR));
+            // console.log(prayerManager.getPrayerLocation());
+            // console.log(prayerManager.getPrayerAdjustmentsByPrayer(prayer.PrayersName.FAJR));
 
-            console.log((prayerManager.getPrayerSettings()as prayer.PrayersSettings).toJSON());
-            console.log(prayerManager.getPrayerSettings());
+            // console.log((prayerManager.getPrayerSettings()as prayer.PrayersSettings).toJSON());
+            // console.log(prayerManager.getPrayerSettings());
+            let prayerView:Array<IPrayersView> = new Array<IPrayersView>();
+            let prayerViewObject:IPrayersView;
+            let prayerTimings: Date[]= new Array<Date>();
+           // const func = (val:prayer.IPrayersTiming)=>{ }
+            //console.log(ramda.project(['prayersDate','prayerTime'],prayerManager.getPrayers()));
+            prayerManager.getPrayers().forEach((curr,index,arr)=>{
+                
+                 curr.prayerTime.forEach((prayerTiming,i)=>{
+                prayerTimings.push(prayerTiming.prayerTime);
+               });
+               prayerView.push(
+                   {
+                       prayerDate: curr.prayersDate,
+                       fajr: prayerTimings[0],
+                       sunrise:prayerTimings[1],
+                       dhuhr:prayerTimings[2],
+                       asr:prayerTimings[3],
+                       sunset:prayerTimings[4],
+                       maghrib:prayerTimings[5],
+                       isha:prayerTimings[6],
+                       midnight:prayerTimings[7]
+                   }
+               )
+            }
+            );
+            // console.log(prayerView);
      //   console.log(DateUtil.getDateByTimeZone(new Date(),'Asia/Dubai'));
    //     console.log(prayerManager.getPrayersByDate(DateUtil.getNowDate()));
     //    console.log(prayerManager.getPrayersByDate(new Date()));
