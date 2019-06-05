bcb2bbaef1a5bea8dbd74ce296769af88b34346c
dpanet <dpanet@hotmail.com>
2019-06-05 17:05:18 +0400

    sentry minimal

--- src/app.ts
+++ src/app.ts
@@ -9,8 +9,8 @@ import moment from "moment";
 import validators =require("./validators/interface.validators");
 //import validators= val.validators;
 import { valid } from "@hapi/joi";
-import util from "util"
 import { ConfigValidator } from "./validators/validator";
+import { DateUtil } from "./util/utility";
 
 let prayers: Array<object> =
     [{ prayerName: 'Fajr', prayerTime: "2019-01-31T05:46:00.000Z" },
@@ -102,13 +102,13 @@ var prayerConfigFE:any= {
 async function buildLocationObject() {
     try {
       //  console.time('Prayer_Manager');
-        //  let prayerConfig: cg.IPrayersConfig = await new Configurator().getPrayerConfig();
-        // let locationConfig: cg.ILocationConfig = await new Configurator().getLocationConfig();
-
+         let prayerConfig: cg.IPrayersConfig = await new Configurator().getPrayerConfig();
+        let locationConfig: cg.ILocationConfig = await new Configurator().getLocationConfig();
+        console.log(DateUtil.getDateByTimeZone(new Date(),"Asia/Dubai"));
         //  console.log(locationConfig);
-        // let prayerManager: managerInterface.IPrayerManager = await manager.PrayerTimeBuilder
-        //     .createPrayerTimeBuilder(locationConfig, prayerConfig)
-        //     .createPrayerTimeManager();
+        let prayerManager: managerInterface.IPrayerManager = await manager.PrayerTimeBuilder
+            .createPrayerTimeBuilder(locationConfig, prayerConfig)
+            .createPrayerTimeManager();
         // let prayerManager: manager.IPrayerManager = await manager.PrayerTimeBuilder
         //     .createPrayerTimeBuilder(null, prayerConfig)
         //     .setLocationByAddress("Abu Dhabi","AE")
@@ -128,7 +128,7 @@ async function buildLocationObject() {
            // let messageShort = message.reduce((prvs,curr,index,array)=> prvs.concat('\r\n',curr));
          //  console.log("Validation Error: "+ validate.getValidationError())
             //console.log(messageShort);
-            //console.log(prayerManager.getPrayersByDate(new Date('2019-06-23')));
+            console.log(prayerManager.getPrayersByDate(new Date('2019-06-23')));
 
     }
     catch (err) {
